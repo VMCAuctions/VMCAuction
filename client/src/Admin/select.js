a10eb6d2cc34501f13c3fe0e65960d1ac3f3f33f
Sree Makam <you@example.com>
2018-01-08 13:37:59 -0800

    shows correct category and fix the edit button issue

--- client/src/Admin/select.js
+++ client/src/Admin/select.js
@@ -11,10 +11,13 @@ class Select extends Component{
         let dropdownOptions = this.props.selectOptions.map((itemValue,index) => {
             return <option key={index} value={itemValue.name} >{itemValue.name}</option>
         })
+        //shows dropdown first option to 'select a category' for new item 
+        //shows dropdown first option to selected category(ex: travel, atrs, wine...) for update item
+            let optionValue = this.props.optionValue === undefined?'Select a category ': this.props.optionValue;
         return(
             <div>
                 <select className='form-control' onChange={this.handleChange} name={this.props.name} >
-                    <option>Select a category</option>
+                    <option>{optionValue}</option>
                     {dropdownOptions}
                 </select>
             </div>
