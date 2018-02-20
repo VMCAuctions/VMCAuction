c90532f38607320d7e58d2f4d28ddec3d22ff9a7
Brandon <brandon.lai00@gmail.com>
2018-02-20 13:57:16 -0800

    added remove last bid to user if they are winning

--- server/controllers/packages.js
+++ server/controllers/packages.js
@@ -300,8 +300,30 @@ this.new = function(req,res){
 		})
 
 	}
-
-	// Item.update({_id: package._items[i]}, { $set: { _package: package._id, packaged: true}}
+	//cancels last bid
+	this.cancel_bid = function(req,res){
+		Package.findById(req.params.id, function(err,package){
+			if(err){
+				console.log(err)
+			}else{
+				var bid	= package.bids[package.bids.length - 1]
+				//console.log('before if statement' + package)
+				if(bid.name === req.session.userName){
+					//console.log('this is the bid' + bid)
+					package.bids.pop()
+					package.save()
+					//console.log('this is the package' + package)
+					
+					//in case client want package not to show on user page 
+					//maybe they want to bid again on the package later
+					//User.findOne({userName: req.session.userName}, function(err, user){
+						
+					//})
+				}
+				res.redirect('/api/users/' + req.session.userName)
+			}
+		})
+	}
 
 }
 
