b02b1b1725da3a86e4228ff147e8cd04450eb629
Lysastrata <celebrian94@gmail.com>
2018-03-26 16:21:34 -0700

    Made the delete item functional

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -151,20 +151,13 @@ function ItemsController(){
 
 	//removing an item
 	this.remove_item = function(req, res){
-		Item.findOne({_id: req.params.id}, function(err, item){
+		Item.remove(item, function(err, result){
 			if(err){
 				console.log(err)
 			}else{
-				console.log('this is result', item)
-				Item.remove(item, function(err, result){
-					if(err){
-						console.log(err)
-					}else{
-						res.redirect('/api/items')
-					}
-				}
-				)}
-		});
+				res.redirect('/api/items')
+			}
+		})		
 	}
 }
 module.exports = new ItemsController();
