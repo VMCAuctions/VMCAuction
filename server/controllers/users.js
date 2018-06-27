aba32697585dc376e1c0f9bbb47eaf15d2722032
corinasm <corina.mitu@yahoo.com>
2018-06-27 15:53:18 -0700

    Updated registration form. Made userName be the email; Deleted the email field from user model

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -10,10 +10,10 @@ function UsersController(){
 		const validationArray = [
 			["firstName", 2, "first name"],
 			["lastName", 2, "last name"],
-			["streetAddress", 2, "street address"],
-			["city", 2, "city"],
-			["states", 2, "state"],
-			["zip", 5, "zip code"],
+			// ["streetAddress", 2, "street address"],
+			// ["city", 2, "city"],
+			// ["states", 2, "state"],
+			// ["zip", 5, "zip code"],
 			["phoneNumber", 10, "phone number"],
 			["password", 6, "password"]
 		];
@@ -124,25 +124,25 @@ function UsersController(){
 						if(err){
 							console.log(err)
 						}else{
-							var validation = registrationValidation(req.body)
-							// email regex validation
-							var emailReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-							var unValidatedEmail = req.body.email;
-							var emailResult = unValidatedEmail.match(emailReg);
-							if (!emailResult){
-								validation += "Invalid email.\n"
-							}
+							// var validation = registrationValidation(req.body)
+							// // email regex validation
+							// var emailReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+							// var unValidatedEmail = req.body.email;
+							// var emailResult = unValidatedEmail.match(emailReg);
+							// if (!emailResult){
+							// 	validation += "Invalid email.\n"
+							// }
 							// userName regex validation based on no spaces in userName
-							var userReg = /^[a-zA-Z0-9\-_.]{5,25}$/;
-							var unValidateduserName = req.body.userName;
-							var userResult = unValidateduserName.match(userReg)
-							if(!userResult){
-								validation += 'Use letters, numbers, and -(dash) or _(underscore) ONLY; between 5-25 characters for userName.\n'
-							}
-							if(validation.length > 0){
-								res.json({validated: false, message: validation})
-								return;
-							}
+							// var userReg = /^[a-zA-Z0-9\-_.]{5,25}$/;
+							// var unValidateduserName = req.body.userName;
+							// var userResult = unValidateduserName.match(userReg)
+							// if(!userResult){
+							// 	validation += 'Use letters, numbers, and -(dash) or _(underscore) ONLY; between 5-25 characters for userName.\n'
+							// }
+							// if(validation.length > 0){
+							// 	res.json({validated: false, message: validation})
+							// 	return;
+							// }
 							//validation is ok, so hash the password and add to the database
 							var lowerUser = req.body.userName.toLowerCase();
 							//In the final product, this will be organizer, but keeping admin for legacy testing.  Also, note that this code isn't being used right now, as admin has an individual create user function run in users.initialize below.
@@ -162,7 +162,7 @@ function UsersController(){
 								firstName: req.body.firstName,
 								lastName: req.body.lastName,
 								phone: req.body.phoneNumber,
-								email: req.body.email,
+								// email: req.body.email,
 								streetAddress: req.body.streetAddress,
 								city: req.body.city,
 								states: req.body.states,
