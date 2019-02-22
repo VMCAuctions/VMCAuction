846d5cc55a522d242380e456faad71d91ca2a7eb
Zachary McManus <zacharymcmanus@Zacharys-MacBook-Pro.local>
2019-02-22 11:00:25 -0800

    removed validations to create empty package

--- server/models/package.js
+++ server/models/package.js
@@ -10,14 +10,14 @@ mongoose.Promise = global.Promise;
 var packageSchema = new Schema({
 	name: {type: String, required: true, minlength: 4},
 	_items: [{type: Number, required: true, ref: 'Item'}],
-	description: {type: String, required: true},
+	description: {type: String},
 	_category: {type: String, required: true},
 	_auctions: {type: Schema.Types.ObjectId, required: true, ref: 'Auction'},
 	value: {type: Number, required: true},  //prepopulated field with for item in package._items { value += item.value }
-	amount: {type: Number, required: true}, //starting bid
+	amount: {type: Number}, //starting bid
 	priority: {type: Number, default: -1}, //varies from 1 - 10, with 1 first
 	featured: {type: Boolean, required: true},
-	bidIncrement: {type: Number, required: true},
+	bidIncrement: {type: Number},
 	bids: {type: Array},
 	// {bidAmount: 50, name: yarik }
 	photo: String,
